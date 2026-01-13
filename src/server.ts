13e5a63bd54b7d5cb3563eec2326dcceffab2c1f
Gabriel-S4 <gabsa2300@gmail.com>
2026-01-13 01:19:27 -0300

    fix ts import

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,21 @@
-import * as http from "http"
-import { app } from './app'
+// import * as http from "http"
+// import { app } from './app'
 
-// trás a função app de app.ts, onde fica a configuração dos métodos e das rotas
-const server = http.createServer(app)
+// // trás a função app de app.ts, onde fica a configuração dos métodos e das rotas
+// const server = http.createServer(app)
 
-// durante o processo, captura o valor PORT de .env e atribui a port
-const port: number = Number(process.env.PORT) || 3333;
-server.listen(port, '0.0.0.0', () => {
+// // durante o processo, captura o valor PORT de .env e atribui a port
+// const port: number = Number(process.env.PORT) || 3333;
+// server.listen(port, '0.0.0.0', () => {
+//   console.log(`Servidor ligado na porta ${port}`);
+// });
+
+import * as http from "http";
+import { app } from "./app.js";
+
+const server = http.createServer(app);
+
+const port = Number(process.env.PORT) || 3333;
+server.listen(port, "0.0.0.0", () => {
   console.log(`Servidor ligado na porta ${port}`);
 });
